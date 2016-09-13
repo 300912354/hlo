56f8c49a0ae2f9da68741753f57f603fed3035d2
300912354 <300912354@PR-A313-19>
2016-09-12 21:48:23 -0400

    Add project files.

---
+++ hlo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hlo.Startup))]
+namespace hlo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
